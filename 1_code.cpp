ca9c7b7ca54e8b3fcac2c60e5ea3a8b58abaa904
Unnati Pawar <unnatipawar@Unnatis-MacBook-Air.local>
2026-09-09 22:41:07 +0530

    Add first C++ program

---
+++ 1_code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int roll = 101;
+    char grade = 'A';
+    float fee = 12500.50;
+
+    cout << "Roll No: " << roll << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Fees: " << fee << endl;
+
+    return 0;
+}
