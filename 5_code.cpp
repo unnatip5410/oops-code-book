2efd1c5d08853a87440075cf2ece16b1509b875b
Unnati Pawar <unnatipawar@Unnatis-MacBook-Air.local>
2026-09-17 11:02:23 +0530

    Added 5 code

---
+++ 5_code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int age;
+
+    void show()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main()
+{
+    Student s1;
+
+    s1.name = "Amit";
+    s1.age = 20;
+
+    s1.show();
+
+    return 0;
+}
