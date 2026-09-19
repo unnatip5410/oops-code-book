9999eb5cf534ba0f5918571d49ce9271051dc317
Unnati Pawar <unnatipawar@Unnatis-MacBook-Air.local>
2026-09-19 11:05:40 +0530

    Add 6th  C++ program

---
+++ 6_code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Demo {
+public:
+Demo() {
+cout << "Constructor called
+";
+}
+~Demo() {
+cout << "Destructor called
+";
+}
+};
+int main() {
+Demo d;
+return 0;
+}
